940777119941384db8e2370d60c169a4809588dc
rrisner-wt <richard.risner@outlook.com>
2021-12-09 20:20:31 +0000

    adding model

---
+++ app/Models/EmployeeTest.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace app.Models
+{
+    public class EmployeeTest
+    {
+        public string Name { get; set; }
+
+        //public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
