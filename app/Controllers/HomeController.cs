a899bba8ef6f2b8cd226bbabcbc07ce71dfd2832
rrisner-wt <richard.risner@outlook.com>
2021-12-09 20:07:09 +0000

    fix using again

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using app.Models;
-using System.Web.Mvc;
+using Microsoft.AspNetCore.Http;
 
 namespace app.Controllers
 {
