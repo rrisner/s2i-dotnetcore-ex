66789f9f0d080728cadb0552924227240f77e086
rrisner-wt <rwrisner@my.waketech.edu>
2021-12-12 03:00:43 +0000

    Fix Mortgage class

--- app/Models/Mortgage.cs
+++ app/Models/Mortgage.cs
@@ -1,21 +1,24 @@
 using System.Collections;
 
-public Mortgage
+namespace app.Models
 {
-    public static List<string> examples = new List<string>();
-    public Mortgage()
+    public class Mortgage
     {
-        Principal = 0;
-        InterestRate = 0;
-        MonthlyPayment = 0;
-        ExtraPayments = 0;
-        ExtraPaymentMonth = 0;
-    }
+        public static List<string> examples = new List<string>();
+        public Mortgage()
+        {
+            Principal = 0;
+            InterestRate = 0;
+            MonthlyPayment = 0;
+            ExtraPayments = 0;
+            ExtraPaymentMonth = 0;
+        }
 
-    public double Principal {get;set;}
-    public double InterestRate {get;set;}
-    public double MonthlyPayment {get;set;}
-    public double ExtraPayment {get;set;}
-    public int ExtraPaymentMonth {get;set;}
+        public double Principal {get;set;}
+        public double InterestRate {get;set;}
+        public double MonthlyPayment {get;set;}
+        public double ExtraPayment {get;set;}
+        public int ExtraPaymentMonth {get;set;}
+    }
 }
 
